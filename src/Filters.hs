b63aef8c90e5bacc3e146848a7c587b5ddaaa0ad
Rose Kunkel <rose@rosekunkel.me>
2019-03-19 09:49:33 -0700

    WIP persistent integration.

---
+++ src/Filters.hs
@@ -0,0 +1,26 @@
+-- | Filters for every field / operation combination.
+
+module Filters
+  ( Filter
+  , userNameEq, userSsnEq, userFriendEq
+  ) where
+
+import qualified Database.Persist
+import Database.Persist ((==.))
+import Model
+
+{-@ data Filter record <r :: Entity record -> Bool, q :: Entity record -> User -> Bool> = _ @-}
+{-@ data variance Filter covariant covariant contravariant @-}
+data Filter record = Filter (Database.Persist.Filter record)
+
+{-@ userNameEq :: val:String -> Filter<{\row -> userName (entityVal row) == val}, {\row v -> entityKey v = userFriend (entityVal row)}> User @-}
+userNameEq :: String -> Filter User
+userNameEq val = Filter (UserName ==. val)
+
+{-@ userSsnEq :: val:Int -> RefinedFilter<{\row -> userSsn (entityVal row) == val}, {\row v -> entityKey v = userId (entityVal row)}> User @-}
+userSsnEq :: Int -> Filter User
+userSsnEq val = Filter (UserSsn ==. val)
+
+{-@ userFriendEq :: val:Int -> RefinedFilter<{\row -> userFriend (entityVal row) == val}, {\row v -> entityKey v = userFriend (entityVal row)}> User @-}
+userFriendEq :: UserId -> Filter User
+userFriendEq val = Filter (UserFriend ==. val)
