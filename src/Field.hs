0542fa15738e9e166c1af2491649abb0211478c9
Rose Kunkel <rose@rosekunkel.me>
2019-07-05 14:42:02 -0700

    WIP using persistent's types.

--- src/Field.hs
+++ src/Field.hs
@@ -2,9 +2,10 @@
 {-@ LIQUID "--no-pattern-inline" @-}
 module Field where
 
+import Data.Functor.Const
 import Data.Text (Text)
 import Data.Aeson (ToJSON, FromJSON)
-import Database.Persist (PersistField, PersistValue, PersistEntity, Key, EntityField, Unique)
+import Database.Persist hiding ((==.), (<-.), selectList) --(PersistField, PersistValue, PersistEntity, Key, EntityField, Unique, Filter, fieldLens, Entity(Entity))
 import qualified Database.Persist
 import qualified Database.Persist.Sqlite
 import qualified Database.Persist.TH
@@ -12,8 +13,11 @@ import qualified Data.Text
 import qualified Data.Proxy
 import qualified Data.Map.Internal
 import qualified GHC.Int
+import Control.Monad.Trans.Class (MonadTrans(..))
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader (ReaderT)
 import Database.Persist.TH (mkPersist, sqlSettings, persistLowerCase)
-
+import Database.Persist.Sql (SqlBackend)
 -- * Models
 -- class PersistEntity record where
 --   data Key record
@@ -30,17 +34,24 @@ data EntityFieldWrapper record typ <policy :: Entity record -> Entity User -> Bo
 data EntityFieldWrapper record typ = EntityFieldWrapper (EntityField record typ)
 {-@ data variance EntityFieldWrapper covariant covariant contravariant invariant invariant @-}
 
+{-@ measure entityKey @-}
+entityKey :: Entity record -> Key record
+entityKey (Entity key _) = key
 
-{-@
-data Entity record = Entity
-  { entityKey :: _
-  , entityVal :: _
-  }
-@-}
-data Entity record = Entity
-  { entityKey :: Key record
-  , entityVal :: record
-  }
+{-@ measure entityVal @-}
+entityVal :: Entity record -> record
+entityVal (Entity _ val) = val
+
+-- {-@
+-- data Entity record = Entity
+--   { entityKey :: _
+--   , entityVal :: _
+--   }
+-- @-}
+-- data Entity record = Entity
+--   { entityKey :: Key record
+--   , entityVal :: record
+--   }
 
 -- ** User
 {-@
@@ -89,6 +100,7 @@ Share
   from UserId
   to UserId
 |]
+
 
 -- mapLeft :: (a -> b) -> Either a c -> Either b c
 -- mapLeft _ (Right x) = Right x
@@ -426,9 +438,6 @@ data Tagged a = Tagged { content :: a }
 
 {-@ data variance Tagged covariant contravariant @-}
 
--- Placeholder for Data.Persistent's Filter type
-data Filter a = Filter
-
 {-@ data RefinedFilter record <r :: Entity record -> Bool, q :: Entity record -> Entity User -> Bool> = RefinedFilter (Filter record) @-}
 data RefinedFilter record = RefinedFilter (Filter record)
 
@@ -446,8 +455,8 @@ forall <policy :: Entity record -> Entity User -> Bool,
   { field :: typ<fieldfilter> |- {v:(Entity <inverseselector field> record) | True} <: {v:(Entity <filter> record) | True} }
   EntityFieldWrapper<policy, selector, inverseselector> record typ -> typ<r> -> RefinedFilter<filter, policy> record
 @-}
-(==.) :: EntityFieldWrapper record typ -> typ -> RefinedFilter record
-field ==. value = undefined
+(==.) :: PersistField typ => EntityFieldWrapper record typ -> typ -> RefinedFilter record
+(EntityFieldWrapper field) ==. value = RefinedFilter (field Database.Persist.==. value)
 
 {-@
 (Field.<-.) ::
@@ -461,8 +470,8 @@ forall <policy :: Entity record -> Entity User -> Bool,
   { field :: typ<fieldfilter> |- {v:(Entity <inverseselector field> record) | True} <: {v:(Entity <filter> record) | True} }
   EntityFieldWrapper<policy, selector, inverseselector> record typ -> [typ<r>] -> RefinedFilter<filter, policy> record
 @-}
-(<-.) :: EntityFieldWrapper record typ -> [typ] -> RefinedFilter record
-field <-. value = undefined
+(<-.) :: PersistField typ => EntityFieldWrapper record typ -> [typ] -> RefinedFilter record
+(EntityFieldWrapper field) <-. value = RefinedFilter (field Database.Persist.<-. value)
 
 {-@
 data FilterList record <q :: Entity record -> Entity User -> Bool, r :: Entity record -> Bool> where
@@ -495,21 +504,26 @@ f ?: fs = f `Cons` fs
 selectList :: forall <q :: Entity record -> Entity User -> Bool, r1 :: Entity record -> Bool, r2 :: Entity record -> Bool, p :: Entity User -> Bool>.
   { row :: record |- {v:(Entity <r1> record) | entityVal v == row} <: {v:(Entity <r2> record) | True} }
   { row :: (Entity <r2> record) |- {v:(Entity <p> User) | True} <: {v:(Entity <q row> User) | True} }
-  FilterList<q, r1> record -> Tagged<p> [(Entity <r2> record)]
+  FilterList<q, r1> record -> ReaderT _ (Tagged<p>) [(Entity <r2> record)]
 @-}
-selectList :: FilterList record -> Tagged [Entity record]
-selectList x = undefined
+selectList :: (PersistQueryRead backend, PersistRecordBackend record backend) => FilterList record -> ReaderT backend Tagged [Entity record]
+selectList filters = Database.Persist.selectList (toPersistFilters filters) []
+  where
+    toPersistFilters Empty = []
+    toPersistFilters (RefinedFilter f `Cons` filters) = f:(toPersistFilters filters)
+
+-- TODO: should Tagged/TIO be a monad transformer? Where should it sit in the stack? Maybe a TaggedIO class or something?
 
 {-@
-projectList :: forall <r1 :: Entity record -> Bool, r2 :: typ -> Bool, policy :: Entity record -> Entity User -> Bool, p :: Entity User -> Bool, selector :: Entity record -> typ -> Bool>.
+assume projectList :: forall <r1 :: Entity record -> Bool, r2 :: typ -> Bool, policy :: Entity record -> Entity User -> Bool, p :: Entity User -> Bool, selector :: Entity record -> typ -> Bool, inverseselector :: typ -> Entity record -> Bool>.
   { row :: (Entity <r1> record) |- {v:(Entity <p> User) | True} <: {v:(Entity <policy row> User) | True} }
   { row :: (Entity <r1> record) |- typ<selector row> <: typ<r2> }
-  EntityFieldWrapper<policy, selector> record typ ->
+  EntityFieldWrapper<policy, selector, inverseselector> record typ ->
   [(Entity <r1> record)] ->
   Tagged<p> [typ<r2>]
 @-}
-projectList :: EntityFieldWrapper record typ -> [Entity record] -> Tagged [typ]
-projectList = undefined
+projectList :: PersistEntity record => EntityFieldWrapper record typ -> [Entity record] -> Tagged [typ]
+projectList (EntityFieldWrapper entityField) entities = Tagged $ map (\x -> getConst $ fieldLens entityField Const x) entities
 
 instance Functor Tagged where
   fmap f (Tagged x) = Tagged (f x)
@@ -536,6 +550,8 @@ instance Monad Tagged where
          -> Tagged<p> b;
      return :: a -> Tagged <{\v -> true}> a
   @-}
+
+instance MonadIO Tagged
 
 -- * Client code
 {-@ measure Field.id1 :: Key User @-}
@@ -567,70 +583,70 @@ exampleList4 = userFriendField ==. id1 ?: userNameField ==. "alice" ?: Empty
 exampleList5 :: FilterList User
 exampleList5 = userNameField ==. "alice" ?: Empty
 
-{-@ exampleSelectList1 :: Tagged<{\v -> entityKey v == id1}> [{v : Entity User | userFriend (entityVal v) == id1}] @-}
-exampleSelectList1 :: Tagged [Entity User]
+{-@ exampleSelectList1 :: ReaderT _ (Tagged<{\v -> entityKey v == id1}>) [{v : Entity User | userFriend (entityVal v) == id1}] @-}
+exampleSelectList1 :: ReaderT SqlBackend Tagged [Entity User]
 exampleSelectList1 = selectList filters
   where
     {-@ filters :: FilterList<{\_ v -> entityKey v == id1}, {\v -> userFriend (entityVal v) == id1}> User @-}
     filters = userFriendField ==. id1 ?: Empty
 
-{-@ exampleSelectList2 :: Tagged<{\v -> entityKey v == id1}> [{v : _ | userFriend (entityVal v) == id1 && userName (entityVal v) == "alice"}] @-}
-exampleSelectList2 :: Tagged [Entity User]
+{-@ exampleSelectList2 :: ReaderT _ (Tagged<{\v -> entityKey v == id1}>) [{v : _ | userFriend (entityVal v) == id1 && userName (entityVal v) == "alice"}] @-}
+exampleSelectList2 :: ReaderT SqlBackend Tagged [Entity User]
 exampleSelectList2 = selectList (userNameField ==. "alice" ?: userFriendField ==. id1 ?: Empty)
 
-{-@ exampleSelectList3 :: Tagged<{\v -> False}> [{v : _ | userName (entityVal v) == "alice"}] @-}
-exampleSelectList3 :: Tagged [Entity User]
+{-@ exampleSelectList3 :: ReaderT _ (Tagged<{\v -> False}>) [{v : _ | userName (entityVal v) == "alice"}] @-}
+exampleSelectList3 :: ReaderT SqlBackend Tagged [Entity User]
 exampleSelectList3 = selectList (userNameField ==. "alice" ?: Empty)
 
-{-@ projectSelect1 :: [{v:_ | userFriend (entityVal v) == id1}] -> Tagged<{\_ -> False}> [{v:_ | len v == 9}] @-}
-projectSelect1 :: [Entity User] -> Tagged [String]
-projectSelect1 users = projectList userSSNField users
+{-@ projectSelect1 :: [{v:_ | userFriend (entityVal v) == id1}] -> ReaderT _ (Tagged<{\_ -> False}>) [{v:_ | len v == 9}] @-}
+projectSelect1 :: [Entity User] -> ReaderT SqlBackend Tagged [String]
+projectSelect1 users = lift $ projectList userSSNField users
 
 -- | This is fine: user 1 can see both the filtered rows and the name field in
 --   each of these rows
-{-@ names :: Tagged<{\v -> entityKey v == id1}> [String]
+{-@ names :: ReaderT _ (Tagged<{\v -> entityKey v == id1}>) [String]
 @-}
-names :: Tagged [String]
+names :: ReaderT SqlBackend Tagged [String]
 names = do
   rows <- selectList (userFriendField ==. id1 ?: Empty)
-  projectList userNameField rows
+  lift $ projectList userNameField rows
 
 -- | This is bad: the result of the query is not public
-{-@ bad1 :: Tagged<{\v -> True}> [{v: _ | userFriend (entityVal v) == id1}]
+{-@ bad1 :: ReaderT _ (Tagged<{\v -> True}>) [{v: _ | userFriend (entityVal v) == id1}]
 @-}
-bad1 :: Tagged [Entity User]
+bad1 :: ReaderT SqlBackend Tagged [Entity User]
 bad1 = selectList (userFriendField ==. id1 ?: Empty)
 
 -- | This is bad: who knows who else has name "alice" and is not friends with user 1?
-{-@ bad2 :: Tagged<{\v -> entityKey v == id1}> [{v: _ | userName (entityVal v) == "alice"}]
+{-@ bad2 :: ReaderT _ (Tagged<{\v -> entityKey v == id1}>) [{v: _ | userName (entityVal v) == "alice"}]
 @-}
-bad2 :: Tagged [Entity User]
+bad2 :: ReaderT SqlBackend Tagged [Entity User]
 bad2 = selectList (userNameField ==. "alice" ?: Empty)
 
 -- | This is bad: user 1 can see the filtered rows but not their SSNs
-{-@ badSSNs :: Tagged<{\v -> entityKey v == id1}> [{v:_ | len v == 9}]
+{-@ badSSNs :: ReaderT _ (Tagged<{\v -> entityKey v == id1}>) [{v:_ | len v == 9}]
 @-}
-badSSNs :: Tagged [String]
+badSSNs :: ReaderT SqlBackend Tagged [String]
 badSSNs = do
   rows <- selectList (userFriendField ==. id1 ?: Empty)
-  projectList userSSNField rows
+  lift $ projectList userSSNField rows
 
 {-@
-getSharedTasks :: u:_ -> Tagged<{\viewer -> entityKey viewer == u}> [{v:_ | len v > 0}]
+getSharedTasks :: u:_ -> ReaderT _ (Tagged<{\viewer -> entityKey viewer == u}>) [{v:_ | len v > 0}]
 @-}
-getSharedTasks :: Key User -> Tagged [String]
+getSharedTasks :: Key User -> ReaderT SqlBackend Tagged [String]
 getSharedTasks userKey = do
   shares <- selectList (shareToField ==. userKey ?: Empty)
-  sharedFromUsers <- projectList shareFromField shares
+  sharedFromUsers <- lift $ projectList shareFromField shares
   sharedTodoItems <- selectList (todoItemOwnerField <-. sharedFromUsers ?: Empty)
-  projectList todoItemTaskField sharedTodoItems
+  lift $ projectList todoItemTaskField sharedTodoItems
 
 {-@
-getSharedTasksBad :: _ -> Tagged<{\viewer -> True}> _
+getSharedTasksBad :: _ -> ReaderT _  (Tagged<{\viewer -> True}>) _
 @-}
-getSharedTasksBad :: Key User -> Tagged [String]
+getSharedTasksBad :: Key User -> ReaderT SqlBackend Tagged [String]
 getSharedTasksBad userKey = do
   shares <- selectList (shareToField ==. userKey ?: Empty)
-  sharedFromUsers <- projectList shareFromField shares
+  sharedFromUsers <- lift $ projectList shareFromField shares
   sharedTodoItems <- selectList (todoItemOwnerField <-. sharedFromUsers ?: Empty)
-  projectList todoItemTaskField sharedTodoItems
+  lift $ projectList todoItemTaskField sharedTodoItems
