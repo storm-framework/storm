708acc02ed00f8e5abf998be7f1632879f8ea811
Nico Lehmann <nico.lehmannm@gmail.com>
2020-05-20 15:15:44 -0700

    Add insertMany

--- Insert.hs
+++ Insert.hs
@@ -3,7 +3,6 @@ module Binah.Insert where
 
 
 import           Binah.Infrastructure
-import           Data.Text
 import           Control.Monad.Reader           ( MonadReader(..)
                                                 , runReaderT
                                                 )
@@ -19,11 +18,11 @@ assume insert :: forall < p :: Entity record -> Bool
                         , querypolicy  :: Entity record -> Entity User -> Bool
                         , audience :: Entity User -> Bool
                         >.
-  { rec :: (Entity<p> record) 
+  { rec :: (Entity<p> record)
       |- {v: (Entity User) | v == currentUser} <: {v: (Entity<insertpolicy rec> User) | True}
   }
 
-  { rec :: (Entity<p> record) 
+  { rec :: (Entity<p> record)
       |- {v: (Entity<querypolicy p> User) | True} <: {v: (Entity<audience> User) | True}
   }
 
@@ -40,3 +39,33 @@ insert
 insert record = do
   backend <- ask
   liftTIO . TIO $ runReaderT (Persist.insert (persistentRecord record)) backend
+
+{-@ ignore insertMany @-}
+{-@
+assume insertMany :: forall < p :: Entity record -> Bool
+                            , insertpolicy :: Entity record -> Entity User -> Bool
+                            , querypolicy  :: Entity record -> Entity User -> Bool
+                            , audience :: Entity User -> Bool
+                            >.
+  { rec :: (Entity<p> record)
+      |- {v: (Entity User) | v == currentUser} <: {v: (Entity<insertpolicy rec> User) | True}
+  }
+
+  { rec :: (Entity<p> record)
+      |- {v: (Entity<querypolicy p> User) | True} <: {v: (Entity<audience> User) | True}
+  }
+
+  [BinahRecord<p, insertpolicy, querypolicy> record]
+  -> TaggedT<{\_ -> True}, audience> _ [Key record]
+@-}
+insertMany
+  :: ( MonadTIO m
+     , Persist.PersistStoreWrite backend
+     , Persist.PersistRecordBackend record backend
+     , MonadReader backend m
+     )
+  => [BinahRecord record]
+  -> TaggedT m [Key record]
+insertMany records = do
+  backend <- ask
+  liftTIO . TIO $ runReaderT (Persist.insertMany (map persistentRecord records)) backend
