b7a154a40a97791fec22738b2c0c0d67a21eddc5
Nico Lehmann <nico.lehmannm@gmail.com>
2020-08-17 18:27:02 -0700

    Remove random-shuffle

--- Random.hs
+++ Random.hs
@@ -1,18 +1,12 @@
-module Binah.Random ( shuffleT ) where
+module Binah.Random where
 
 import qualified Control.Monad.Random          as Random
 
 import           Binah.Core
 import           Binah.Infrastructure
-import qualified System.Random.Shuffle         as Shuffle
-
 
 instance Random.MonadRandom TIO where
   getRandom   = TIO Random.getRandom
   getRandoms  = TIO Random.getRandoms
   getRandomR  = TIO . Random.getRandomR
   getRandomRs = TIO . Random.getRandomRs
-
-{-@ assume shuffleT :: [a] -> TaggedT<{\_ -> True}, {\_ -> False}> user m [a] @-}
-shuffleT :: MonadTIO m => [a] -> TaggedT user m [a]
-shuffleT = liftTIO . Shuffle.shuffleM
